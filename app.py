b1c05a9894916a4658f63bc4db818b282627d24c
Yash Chouhan <48046446+iamyashchouhan@users.noreply.github.com>
2024-03-19 21:21:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def start():
     return "The MBSA Server is Running"
+
+with open("data.json", "r") as f:
+    language_mappings = json.load(f)
+
+@app.route("/languages", methods=["GET"])
+def get_languages():
+    return jsonify(language_mappings)
 
 @app.route("/mbsa")
 def mbsa():
